c6db74e07399f3b20ea68d8b53089432e0cbbc2d
Ashiq Uddin Pranto <ashiqpranto@gmail.com>
2022-11-20 10:02:18 +0600

    SplashScreen and homepage added

---
+++ lib/splashScreen.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+import 'package:exam/homePage.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    Timer(Duration(seconds: 3),(){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>homePage()));
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: Center(
+        child: Text("HamroMausam",
+          style: TextStyle(
+            fontSize: 50
+          ),
+        ),
+      ),
+    );
+  }
+}
+
