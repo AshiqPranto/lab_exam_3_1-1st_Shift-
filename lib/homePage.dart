02b04234a837989127b2fede65ddae5950c84e8e
Ashiq Uddin Pranto <ashiqpranto@gmail.com>
2022-11-20 10:11:59 +0600

    added border circle to homepage which will contain our widget

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -12,7 +12,15 @@ class _homePageState extends State<homePage> {
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Text("homepage"),
+        child: Container(
+          width: 500,
+          height: 800,
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black,width: 4),
+            borderRadius: BorderRadius.all(Radius.circular(200)),
+
+          ),
+        ),
       ),
     );
   }
